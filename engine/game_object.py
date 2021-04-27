1579ba7df5919a0906c855906c3fe5e75c005d23
Rodney Dev <83191081+rodevrel@users.noreply.github.com>
2021-04-27 18:01:09 -0500

    Create game_object.py

---
+++ engine/game_object.py
@@ -0,0 +1,54 @@
+import abc from ABC
+
+# Transform Game Objects
+class Transform:
+  
+  def __init__(self, x=0, y=0, width=0, height=0, scale_x=1, scale_y=1):
+    
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
+    self.scale_x= scale_X
+    self.scale_y= scale_y
+    
+  @property
+  def center(self):
+    x = self.x + (self.width / 2)
+    y = self.y + (self.height / 2)
+    
+    return x, y
+  
+  def change_position(self, x, y):
+    self.x = x or self.x
+    self.y = y or self.y
+    
+  def change_dimension(self, width, height):
+    self.width = width or self.width
+    self.height = height or self.height
+    
+  def change_scale(self, sx, sy):
+    self.scale_x = sx + self.scale_x
+    self.scale_y = sy + self.scale_y
+    
+# Game Objects
+class AbsractGameObject(ABC):
+  
+  def __init__(self, transform: Transform, tag: str = None, name: str = None):
+    
+    self.__transform__: Transform = transform or Transform()
+    self.__tag__ = tag
+    self.__name__ = name
+    
+  @property
+  def transform(self):
+    return self.__transform__
+  
+  @property
+  def tag(self):
+    return self.__tag__
+  
+  @property
+  def name(self):
+    return self.__name__
+    return self.
